0dd1fe3f9948c265193822d372cd9b9b8d840f84
Tim.M <mazzolinitim@gmail.com>
2023-05-19 19:46:16 +0200

    v1.3 Cisco OneAccess logo

--- app/src/main/java/dev/azertim/orange_toolbox/ui/reseau/calculatrice/CalculatriceFragment.java
+++ app/src/main/java/dev/azertim/orange_toolbox/ui/reseau/calculatrice/CalculatriceFragment.java
@@ -1,6 +1,7 @@
 package dev.azertim.orange_toolbox.ui.reseau.calculatrice;
 
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,13 +25,13 @@ public class CalculatriceFragment extends Fragment {
     private TextView tvSubnetMask, tvInvertedMask, tvFirstAddress, tvLastAddress, tvHostCount;
 
     public CalculatriceFragment() {
-        // Required empty public constructor
+        // Constructeur par défaut requis pour les fragments
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflater le layout pour ce fragment
         return inflater.inflate(R.layout.fragment_calculatrice, container, false);
     }
 
@@ -38,6 +39,7 @@ public class CalculatriceFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Initialisation des vues (EditText, Button, TextView)
         etNetworkAddress = view.findViewById(R.id.networkAddressEditText);
         btnCalculate = view.findViewById(R.id.calculateButton);
         tvSubnetMask = view.findViewById(R.id.subnetMaskTextView);
@@ -46,6 +48,7 @@ public class CalculatriceFragment extends Fragment {
         tvLastAddress = view.findViewById(R.id.lastAddressTextView);
         tvHostCount = view.findViewById(R.id.hostCountTextView);
 
+        // Définition de l'action à effectuer lors du clic sur le bouton "Calculer"
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +58,7 @@ public class CalculatriceFragment extends Fragment {
     }
 
     private void calculateNetworkInfo() {
+        // Récupération de l'adresse réseau avec le masque saisie par l'utilisateur
         String networkAddressWithMask = etNetworkAddress.getText().toString().trim();
 
         if (networkAddressWithMask.isEmpty()) {
@@ -63,21 +67,29 @@ public class CalculatriceFragment extends Fragment {
         }
 
         try {
+            // Séparation de l'adresse IP et du masque
             String[] parts = networkAddressWithMask.split("/");
             String ipAddress = parts[0];
             int maskBits = Integer.parseInt(parts[1]);
 
+            // Conversion de l'adresse IP en objet InetAddress
             InetAddress networkAddress = InetAddress.getByName(ipAddress);
             int address = byteArrayToInt(networkAddress.getAddress());
 
+            // Calcul du masque et du masque inversé
             int mask = (0xFFFFFFFF << (32 - maskBits)) & 0xFFFFFFFF;
             int invertedMask = ~mask & 0xFFFFFFFF;
 
+            // Calcul de la première adresse utilisable pour un hôte
             int firstAddress = (address & mask) + 1;
+
+            // Calcul de la dernière adresse utilisable pour un hôte (adresse de diffusion)
             int lastAddress = (address | ~mask) - 1;
 
+            // Calcul du nombre d'hôtes
             int hostCount = lastAddress - firstAddress + 1;
 
+            // Affichage des résultats dans les TextView correspondants
             tvSubnetMask.setText(intToIpAddress(mask));
             tvInvertedMask.setText(intToIpAddress(invertedMask));
             tvFirstAddress.setText(intToIpAddress(firstAddress));
@@ -87,9 +99,14 @@ public class CalculatriceFragment extends Fragment {
             e.printStackTrace();
         } catch (NumberFormatException e) {
             etNetworkAddress.setError("Invalid network address format");
+        } catch (ArrayIndexOutOfBoundsException e){
+            etNetworkAddress.setError("Invalid network address format");
+        } catch (NetworkOnMainThreadException e) {
+            etNetworkAddress.setError("Invalid network address format");
         }
     }
 
+    // Conversion d'un tableau de bytes en entier
     private int byteArrayToInt(byte[] bytes) {
         int value = 0;
         for (int i = 0; i < bytes.length; i++) {
@@ -98,6 +115,7 @@ public class CalculatriceFragment extends Fragment {
         return value;
     }
 
+    // Conversion d'un entier en adresse IP au format String
     private String intToIpAddress(int value) {
         return ((value >> 24) & 0xFF) + "." +
                 ((value >> 16) & 0xFF) + "." +
